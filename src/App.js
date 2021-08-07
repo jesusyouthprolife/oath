865be31cd08dde53732b1556abc4192d418a772a
Amal <howareyouami@gmail.com>
2021-08-07 10:22:03 +0530

    add cname

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Prolife oath
+         Prolife oath 3
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Do oath
         </a>
       </header>
     </div>
