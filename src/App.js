33eea29a54af41b92f04961370b1c1e157f0f168
Amal <howareyouami@gmail.com>
2021-08-07 10:45:54 +0530

    Style fixes

--- src/App.js
+++ src/App.js
@@ -5,18 +5,54 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         Prolife oath 4
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Do oath
-        </a>
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1>PROLIFE OATH</h1>
+      <p className='oath'>
+        As a member of the human race, the Crown of God's creation, 
+
+        I (say full name) solemnly pledge my life to the service of God and humanity;
+
+        I will obey the Word ofGod and live in accordance with the Magisterial teachings of the Catholic Church;
+
+        I will lead my life according to God's wonderful plan about love, sexuality, celibacy, marriage, family & human life;
+
+        I will maintain the utmost respect for human life from the time of conception till natural death;
+
+        I will not permit the evils of contraception, sterilisation, abortion, artificial reproductive technology & euthanasia;
+
+        I make these promises solemnly, freely, and upon my honour.
+      </p>
+      <form className='main-form' onSubmit={(e)=>{
+          e.preventDefault();
+          alert('Submitted!')
+        }}>
+        <label>
+          Name:
+          <input required type="text" name="name" />
+        </label>
+        <label>
+          Email:
+          <input required type="text" name="email" />
+        </label>
+               <label>
+          WhatsApp:
+          <input type="text" name="name" />
+        </label>
+               <label>
+          Mobile:
+          <input type="text" name="name" />
+        </label>
+
+               <label>
+          State:
+          <input type="text" name="name" />
+        </label>
+                       <label>
+          Country:
+          <input type="text" name="name" />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
       </header>
     </div>
   );
