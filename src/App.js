5f29ec1cfa2b5812cf91a7c300cf84a465d12908
Amal <howareyouami@gmail.com>
2021-08-14 10:50:03 +0530

    Add new layout

--- src/App.js
+++ src/App.js
@@ -1,69 +1,16 @@
-import logo from "./prolife-logo.jpeg";
 import "./App.css";
-import { sendFeedback } from "./api";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>PROLIFE OATH</h1>
-        <p className="oath">
-          As a member of the human race, the Crown of God's creation, I (say
-          full name) solemnly pledge my life to the service of God and humanity;
-          I will obey the Word ofGod and live in accordance with the Magisterial
-          teachings of the Catholic Church; I will lead my life according to
-          God's wonderful plan about love, sexuality, celibacy, marriage, family
-          & human life; I will maintain the utmost respect for human life from
-          the time of conception till natural death; I will not permit the evils
-          of contraception, sterilisation, abortion, artificial reproductive
-          technology & euthanasia; I make these promises solemnly, freely, and
-          upon my honour.
-        </p>
-        <form
-          className="main-form"
-          onSubmit={(e) => {
-            e.preventDefault();
-            sendFeedback({
-              name: "Test",
-            })
-              .then(() => {
-                alert("Submitted!");
-              })
-              .catch(() => {
-                alert("Failed!");
-              });
-          }}
-        >
-          <label>
-            Name:
-            <input required type="text" name="name" />
-          </label>
-          <label>
-            Email:
-            <input required type="text" name="email" />
-          </label>
-          <label>
-            WhatsApp:
-            <input type="text" name="name" />
-          </label>
-          <label>
-            Mobile:
-            <input type="text" name="name" />
-          </label>
-
-          <label>
-            State:
-            <input type="text" name="name" />
-          </label>
-          <label>
-            Country:
-            <input type="text" name="name" />
-          </label>
-          <input type="submit" value="Submit" />
-        </form>
-        <div className="stats">Total 200 persons have done the oath</div>
-      </header>
+    <div className="container">
+      <div className="content">
+        <div className="left">
+          <div className="circle"></div>
+        </div>
+        <div className="right">
+          <p className="heading-text">#neverforgotten</p>
+        </div>
+      </div>
     </div>
   );
 }
