09b7fcb00b5808574d99af43fa1fa6a47f0796da
Amal <howareyouami@gmail.com>
2021-08-15 15:28:07 +0530

    footer responsive

--- src/Components/Layout.js
+++ src/Components/Layout.js
@@ -1,17 +1,23 @@
 function Layout({ left, right }) {
   return (
     <>
-    <div className="container">
-      <div className="content">
-        <div className="left">{left}</div>
-        <div className="right">{right}</div>
+      <div className="container">
+        <div className="content">
+          <div className="left">{left}</div>
+          <div className="right">{right}</div>
+        </div>
+        <div className="footer">
+          <b>Contact us</b>
+          <span>&bull;</span>
+          <br />
+          <b>Email:</b>{" "}
+          <a href="mailto:jyindiaprolife@gmail.com">jyindiaprolife@gmail.com</a>
+          <span>&bull;</span>
+          <br />
+          <b>Phone / Whatsapp:</b>{" "}
+          <a href="tel:+91 988-604-2540">+91 988-604-2540</a>
+        </div>
       </div>
-      <div className="footer">
-      <b>Contact us</b><span>&bull;</span>
-      <b>Email:</b> <a href="mailto:jyindiaprolife@gmail.com">jyindiaprolife@gmail.com</a><span>&bull;</span>
-      <b>Phone / Whatsapp:</b> <a href="tel:+91 988-604-2540">+91 988-604-2540</a>
-    </div>
-    </div>
     </>
   );
 }
